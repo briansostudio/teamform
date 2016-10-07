2563c028adcf634ff159ef9f8a73f57b08eb7095
Kristopher Lam <kristopher@bull-b.com>
2016-10-07 15:35:50 +0800

    basic template for demostrating usage of vue

---
+++ src/components/EventAction.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="large ui buttons">
+        <button class="ui positive button" v-on:click="say">{{createEventTitle}}</button>
+        <div class="or"></div>
+        <button class="ui button">{{manageEventTitle}}</button>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      createEventTitle: 'Create Event',
+      manageEventTitle: 'Manage Your Event'
+    }
+  },
+  methods: {
+  	say: function(){
+  		console.log(this.eventName);
+  	}
+  },
+  props: ['eventName']
+}
+</script>
