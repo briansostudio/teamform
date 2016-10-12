eff630aac6d85a072c07d47c1d09a83a31c08853
SILVER PANG <secretsilver@ymail.com>
2016-10-12 14:05:40 +0800

    Added Radar.vue

---
+++ src/components/Radar.vue
@@ -0,0 +1,14 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+export default {
+	watch: {
+		size: function(value){
+			this.$emit('valueChanged')
+		}
+	},
+	props: ['type', 'size']
+}
+</script>
