aaa2d0bbbb4412d73d52a62c4fa124c29af3842b
Brian So <briansostudio@gmail.com>
2016-11-04 16:20:13 +0800

    added unit test for Schedule and TimeInterval

---
+++ src/lib/schedule/schedule.js
@@ -0,0 +1,12 @@
+/**
+ * Created by brianso on 4/11/2016.
+ */
+import TimeInterval from './time-interval'
+
+class Schedule{
+  constructor(intervals){
+    this.intervals = intervals || [];
+  }
+}
+
+export default Schedule;
---
+++ src/lib/schedule/time-interval.js
@@ -0,0 +1,7 @@
+class TimeInterval {
+  constructor(t1, t2){
+    this.t1 = t1;
+    this.t2 = t2;
+  }
+}
+export default TimeInterval;
