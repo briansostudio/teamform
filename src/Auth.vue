a427abd31ace71ec15503511000f3f49a62a1d90
Clement Ng <ClementNg@Clements-MacBook-Air.local>
2016-11-05 21:31:09 +0800

    first version login page layout

---
+++ src/Auth.vue
@@ -0,0 +1,97 @@
+<template>
+  <form class="auth-form" v-on:submit.prevent="signUpStatue ? signUpWithPassword() : signInWithPassword()">
+    <h1>{{signUpStatue ? 'Sign up' : 'Sign in'}}</h1>
+    <div class="container">
+      <div class="ui input">
+      	<input type="email" name="email" id="email" placeholder="Email" required v-model.trim="email">
+      </div>
+    </div>
+    <div class="container">
+      <div class="ui input">
+      	<input type="password" name="password" id="password" required v-model.trim="password">
+      </div>
+    </div>
+    <div v-show="signUpStatue" class="container">
+      <div class="ui input">
+      	<input type="password" name="confirm-password" id="confirm-password" v-model.trim="confirmPassword">
+      </div>
+    </div>
+    <div v-show="!signUpStatue" class="clearfix btn-group">
+      <button class="ui button" type="submit">Sign in</button>
+      <button class="ui button" type="button" v-on:click="signUpStatue = true">Sign up</button>
+    </div>
+    <div v-show="signUpStatue">
+      <button type="submit" class="ui button signup-submit">Sign up</button>
+    </div>
+    <hr>
+    <div class="social-providers">
+      <a href="#" v-on:click.prevent="signInWithProvider('facebook')"><i class="fa fa-facebook-square" aria-hidden="true" style="font-size:48px"></i></a>
+      <a href="#" v-on:click.prevent="signInWithProvider('twitter')"><i class="fa fa-twitter-square" aria-hidden="true" style="font-size:48px"></i></a>
+      <a href="#" v-on:click.prevent="signInWithProvider('google')"><i class="fa fa-google-plus-square" aria-hidden="true" style="font-size:48px"></i></a>
+      <a href="#" v-on:click.prevent="signInWithProvider('github')"><i class="fa fa-github-square" aria-hidden="true" style="font-size:48px"></i></a>
+    </div>
+  </form>
+</template>
+<script>
+export default 
+{
+    data () {
+      return {
+        email: '',
+        password: '',
+        confirmPassword: '',
+        signUpStatue: false,
+      }
+    },
+    methods: {
+
+    }
+}
+  
+</script>
+
+<style>
+  .auth-form{
+    width: 500px;
+    max-width: 100%;
+    margin: 25vh auto 15px;
+    background: #fff;
+    padding: 20px;
+    border-radius: 2px;
+    box-shadow: 0 0px 0px #ccc;
+  }
+  .container
+  {
+	margin: auto;
+    width: 100%;
+  }
+
+  .auth-form h1{
+    font-weight: 500;
+  }
+  .auth-form > div {
+    margin-top: 15px;
+  }
+  button.ui.button {
+    font-size: 18px;
+    background: #41b883;
+    color: #fff;
+    width: 45%;
+    margin-left: 10px;
+  }
+  .ui.input
+  {
+  	 width: 100%;
+  }
+  .auth-form hr{
+    margin-top: 20px;
+  }
+  .auth-form .social-providers{
+    text-align: center;
+  }
+  .auth-form .social-providers a{
+    color: #41b883;
+    font-size: 36px;
+    padding: 4px;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,20 +1,31 @@
 import Vue from 'vue'
 import App from './App'
 import Event from './Event'
+import Auth from './Auth'
 import VueRouter from 'vue-router'
 import VueFire from 'vuefire'
 import Firebase from 'firebase'
+import AuthMethod from './AuthMethod'
 
 Vue.use(VueRouter)
 Vue.use(VueFire)
 
+// const fb = Firebase.initializeApp({
+// 	apiKey: "AIzaSyB37sX4oAPk10vSdiUdmWehnnGJf4KXk-8",
+// 	authDomain: "teamform-14254.firebaseapp.com",
+// 	databaseURL: "https://teamform-14254.firebaseio.com",
+// 	storageBucket: "teamform-14254.appspot.com",
+// 	messagingSenderId: "250682606003"
+// })
+
 const fb = Firebase.initializeApp({
-	apiKey: "AIzaSyB37sX4oAPk10vSdiUdmWehnnGJf4KXk-8",
-	authDomain: "teamform-14254.firebaseapp.com",
-	databaseURL: "https://teamform-14254.firebaseio.com",
-	storageBucket: "teamform-14254.appspot.com",
-	messagingSenderId: "250682606003"
+    apiKey: "AIzaSyBRhkehD5v30D1IyP4Wc7QF5FyML6AAasg",
+    authDomain: "comp3111-266ca.firebaseapp.com",
+    databaseURL: "https://comp3111-266ca.firebaseio.com",
+    storageBucket: "comp3111-266ca.appspot.com",
+    messagingSenderId: "1061138167888"
 })
+
 
 const db = fb.database()
 
@@ -28,6 +39,11 @@ const routes = [
 		path: '/event/:id',
 		name: 'event',
 		component: Event
+	},
+	{
+		path: '/auth',
+		name: 'auth',
+		component: Auth
 	}
 ]
 
@@ -43,6 +59,6 @@ const app = new Vue({
 		events: db.ref('/events')
 	},
 	data: {
-		db : db
+		db : db,
 	}
 }).$mount('#app')
