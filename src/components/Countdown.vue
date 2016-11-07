a6dec10cbfe6032618e0db9c3ec71d15efd6d8fd
Clement Ng <ng2b30@hotmail.com>
2016-11-07 18:34:21 +0800

    Added basic deadline counter

---
+++ src/components/Countdown.vue
@@ -0,0 +1,122 @@
+<template>
+
+        <div id="clockdiv">
+                <h1 v-if="eventDate-now < 0">Event ended</h1>
+                <h1 v-if="eventDate-now > 0">Time remaining:</h1>
+        <div class="block">
+            <span class="digit">{{ days }}</span>
+            <div class="text">Days</div>
+        </div>
+        <div class="block">
+            <span class="digit">{{ hours }}</span>
+            <div class="text">Hours</div>
+        </div>
+        <div class="block">
+            <span class="digit">{{ minutes }}</span>
+            <div class="text">Minutes</div>
+        </div>
+        <div class="block">
+            <span class="digit">{{ seconds }}</span>
+            <div class="text">Seconds</div>
+        </div>
+    </div>
+    
+</template>
+<script>
+export default {
+
+    data() {
+        return {
+            now: Math.trunc((new Date()).getTime() / 1000),
+            eventDate: Math.trunc(Date.parse(this.Date) / 1000)
+        }
+    },
+    ready() {
+       
+    },
+    props:['Date'],
+    computed: {
+
+            seconds() {
+                if(this.eventDate - this.now > 0)
+                {
+                    window.setInterval(() => {this.now = Math.trunc((new Date()).getTime() / 1000)},1000);  
+                    return (this.eventDate - this.now) % 60;
+                }
+                else
+                {
+                    return 0;
+                }
+            },
+            minutes() {
+                if(this.eventDate - this.now >0)
+                {
+                    return Math.trunc((this.eventDate - this.now) / 60) % 60;
+                }
+                else
+                {
+                    return 0;
+                }
+            },
+            hours() {
+
+                if(this.eventDate - this.now >0)
+                {
+                   return Math.trunc((this.eventDate - this.now) / 60 / 60) % 24;
+                }
+                else
+                {
+                    return 0;
+                }
+            },
+            days() {
+
+                if(this.eventDate - this.now >0)
+                {
+                    return Math.trunc((this.eventDate - this.now) / 60 / 60 / 24);
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+    }
+}
+
+</script>
+
+<style>
+
+
+h1{
+  color: #396;
+  font-weight: 100;
+  font-size: 20px;
+  margin: 40px 0px 20px;
+}
+
+#clockdiv{
+	color: #fff;
+	display: inline-block;
+	font-weight: 200;
+	text-align: center;
+	font-size: 16px;
+}
+
+#clockdiv > div{
+	padding: 15px;
+	border-radius: 4px;
+	background: #00BF96;
+	display: inline-block;
+}
+
+#clockdiv div > span{
+	padding: 20px;
+	border-radius: 3px;
+	background: #00816A;
+	display: inline-block;
+}
+
+
+
+</style>
