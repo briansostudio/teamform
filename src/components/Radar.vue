a1dcff78b25ef4c6ee793e15311d6f0c6e3abb0a
SILVER PANG <secretsilver@ymail.com>
2016-11-07 14:18:50 +0800

    updated Radar component using vue-chartjs

--- src/components/Radar.vue
+++ src/components/Radar.vue
@@ -1,14 +1,26 @@
-<template>
-	<div></div>
-</template>
-
 <script>
-export default {
-	watch: {
-		size: function(value){
-			this.$emit('valueChanged')
-		}
-	},
-	props: ['type', 'size']
-}
+import VueCharts from 'vue-chartjs'
+import { Radar } from 'vue-chartjs'
+
+export default Radar.extend({
+  mounted () {
+    // Overwriting base render method with actual data.
+
+    this.renderChart({
+      labels: ["Web", "Android", "iOS", "UI Design", "Server", "Custom 1", "Custom 2"],
+      datasets: [
+        {
+          label: "Andrew",
+            backgroundColor: "rgba(179,181,198,0.2)",
+            borderColor: "rgba(179,181,198,1)",
+            pointBackgroundColor: "rgba(179,181,198,1)",
+            pointBorderColor: "#fff",
+            pointHoverBackgroundColor: "#fff",
+            pointHoverBorderColor: "rgba(179,181,198,1)",
+            data: [65, 59, 90, 81, 56, 55, 40]
+        }
+      ]
+    })
+  }
+})
 </script>
