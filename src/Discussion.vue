3828e5157f916447adc901876cdcd37a76432f58
ttchiuaa <daviddach917@gmail.com>
2016-11-11 13:29:48 +0800

    a simple disscussion board prototype

---
+++ src/Discussion.vue
@@ -0,0 +1,20 @@
+<template>
+    <DiscussionBoard></DiscussionBoard>
+</template>
+
+<script>
+    import DiscussionBoard from './components/DiscussionBoard'
+
+    export default{
+        components:{
+            DiscussionBoard
+        }
+    }
+</script>
+
+<style>
+    h1.ui.center.aligned.icon.header{
+  color: #00FA9A;
+  font-family: 'Varela Round', sans-serif;
+}
+</style>
---
+++ src/components/DiscussionBoard.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+    <h1 class="ui center aligned icon header">
+	  <i class="comments icon"></i>
+	  Discussion Board
+	</h1>
+    <div class="ui segment">
+        <p>
+                <div class="ui feed">
+                    <p>
+                        <div class="event">
+                            <div class="content">
+                                <div class="summary">
+                                    <a class="user">
+                                    Elliot Fu
+                                    </a>
+                                    <div class="date">
+                                    1 Hour Ago
+                                    </div>
+                                    <div class="extra text">
+                                        Hi !
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </p>
+                </div>
+        </p>
+    </div>
+    <div class="ui fluid action input">
+        <input placeholder="Message" type="text">
+        <div class="ui button">Send</div>
+    </div>
+    </div>
+</template>
+
+<script>
+    
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
