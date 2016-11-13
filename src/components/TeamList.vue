f19b8a8a1ab3ac0d8dc66f79275efb7585410d1a
singspiel <ckw1234.sw@gmail.com>
2016-11-13 15:40:15 +0800

    change team list display

--- src/components/TeamList.vue
+++ src/components/TeamList.vue
@@ -1,32 +1,30 @@
 <template>
 	<div class="ui centered grid">
 		<div class="column">
-			<table class="ui definition table">
+			<table class="ui table">
 				<thead>
 					<tr>
-						<th></th>
-						<th class="center aligned">Team ID</th>
+						<th class="center aligned">Team Name</th>
 						<th class="center aligned">Preferred team size</th>
-						<th class="center aligned">Current team members</th>
+						<th class="center aligned">Request to join team</th>
 						<th class="center aligned">Team Page</th>
 					</tr>
 				</thead>
 				<tbody>
 					<tr v-for="(team, index) in teams">
-						<td>{{team.name}}</td>
-						<td class="center aligned">{{index}}</td>
+						<td class="center aligned">{{team.name}}</td>
 						<td class="center aligned">
-							<a class="ui teal circular label">{{team.size}}</a>
+							<a class="ui circular label">{{team.size}}</a>
 						</td>
-						<td class="center aligned">
-							<button class="ui labeled icon button">
-								<i class="unhide icon"></i>
-								View
+						<td class="center aligned"> <!-- function for member -->
+							<button class="ui teal labeled icon button ">
+								<i class="add circle  icon"></i>
+								Join team
 							</button>
 						</td>
-						<td class="center aligned">
+						<td class="center aligned"> <!-- function for team leader -->
 							<button class="ui labeled icon button">
-								<i class="add user icon"></i>
+								<i class="setting icon"></i>
 								Manage
 							</button>
 						</td>
@@ -42,3 +40,9 @@ export default {
 	props: ['teams']
 }
 </script>
+
+<style>
+a.ui.label:hover, a.ui.labels .label:hover{
+	cursor: default;
+}
+</style>
