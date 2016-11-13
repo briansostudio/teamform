4122bbbaf761a91b557a907ede51b35034eaee18
Kristopher Lam <kristopher@bull-b.com>
2016-11-13 19:26:04 +0800

    patched merge produced issues

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import { sync } from 'vuex-router-sync'
 import routes from './routes'
+import store from './store'
 
 import 'sweetalert2/dist/sweetalert2.min.css';
 
@@ -16,4 +17,5 @@ sync(store, router)
 
 const app = new Vue({
   router,
-});
+  store
+}).$mount('#app');
