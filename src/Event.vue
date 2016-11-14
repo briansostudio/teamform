692e8de4b3a39a88887e4e5ea24d7cd22adc706b
Clement Ng <ng2b30@hotmail.com>
2016-11-14 17:59:05 +0800

    fixed some typo

--- src/Event.vue
+++ src/Event.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="ui centered grid">
-		<BasicUserStatus></basicuserstatus>
+		<BasicUserStatus></BasicUserStatus>
 		<div class="row event-page">
 			<h1>Event Name: {{event.name}}</h1>
 		</div>
@@ -64,7 +64,7 @@
 import TeamSizeControl from './components/TeamSizeControl'
 import TeamList from './components/TeamList'
 import swal from 'sweetalert2'
-import basicuserstatus from './components/BasicUserStatus'
+import BasicUserStatus from './components/BasicUserStatus'
 
 export default {
 	created: function(){
