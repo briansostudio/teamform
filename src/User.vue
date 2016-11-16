4c791f6892da9954b86bf2c6deb3096f93eb8cee
Leo <leolo322@yahoo.com.hk>
2016-11-16 11:29:57 +0800

    improvement in MyRequest and EventList

--- src/User.vue
+++ src/User.vue
@@ -1,19 +1,22 @@
 <template>
     <div class="User">
       <WeeklyScheduleTest></WeeklyScheduleTest>
+      <MyRequests :request = 'request'></MyRequests>
     </div>
 </template>
 
 <script>
   import WeeklyScheduleTest from './components/WeeklySchedule/WeeklyScheduleTest.vue'
+  import MyRequests from './components/MyRequests.vue'
   export default {
       data(){
-          return {}
-      },
+          return {
+            request: [{event: 'Open New Issue', team: 'tim team'}, {event: 'Pull Request', team: 'Team Tim'}]
+          },
       computed: {},
       methods: {},
       props: {},
-      components: {WeeklyScheduleTest}
+      components: {WeeklyScheduleTest, MyRequests}
   }
 </script>
 
---
+++ src/components/MyRequests.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2 class = "header">List of my Request</h2>
+  <div class = "request list" v-for="(data, index) in events.teamName">
+    <el-row type="flex" class="row-bg" justify="space-around">
+      <el-col :span="6"><div class="index">{{index+1}}.</div></el-col>
+      <el-col :span="6"><div class="request data"> <b>Event:</b> {{data.event}}  <b>Team:</b> {{data.team}}</div></el-col>
+      <el-col :span="6"><div class="cancel button">
+        <button class="ui labeled icon button">
+          <i class="fa fa-times"></i>
+            Cancel Request
+          </button>
+      </div></el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['requests']
+}
+</script>
+
