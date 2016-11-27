a2021152fd844e0dfaa39d46bad8ecb14eb55348
Brian So <briansostudio@gmail.com>
2016-11-28 01:21:06 +0800

    user ui improvement

--- src/components/RadarSlideInput.vue
+++ src/components/RadarSlideInput.vue
@@ -1,7 +1,7 @@
 <template>
     <el-row>
       <div>
-        <Slider v-for="(n, index) in viewingUser.radarChartData.labels" :index="index" :criterion="viewingUser.radarChartData.datasets[0].data[index]">
+        <Slider v-for="(label, index) in currentEvent.criteria" :label="label" :index="index" :criterion="viewingUser.radarChartData.datasets[0].data[index]">
         </Slider>
       </div>
     </el-row>
@@ -13,7 +13,7 @@ import Slider from './Slider'
 
 export default ({
   computed: {
-    ...mapGetters(['viewingUser'])
+    ...mapGetters(['viewingUser','currentEvent'])
   },
   components: {Slider}
 })
