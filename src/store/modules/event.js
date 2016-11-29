ee78e1c7a0e59c5bf49e7f25d3e44bbe49a48a92
Brian So <briansostudio@gmail.com>
2016-11-29 20:05:36 +0800

    fix tags

--- src/store/modules/event.js
+++ src/store/modules/event.js
@@ -155,14 +155,14 @@ const actions = {
             commit(types.EVENT_NAME_UPDATED, payload)
         })
     },
-    addRecruitCriteria({commit}, payload){
+    addRecruitCriteria({commit, state}, payload){
         let updatedCriteria = state.criteria.map(a=>a);
         updatedCriteria.push(payload)
         api.event.updateEvent(state.id, {criteria: updatedCriteria}).then( () => {
             //commit(types.EVENT_ADD_RECRUIT_CRITERIA, payload)
         })
     },
-    discardRecruitCriteria({commit}, payload){
+    discardRecruitCriteria({commit, state}, payload){
         let updatedCriteria = state.criteria.map(a=>a);
         updatedCriteria.splice(updatedCriteria.indexOf(payload), 1)
         api.event.updateEvent(state.id, {criteria: updatedCriteria}).then( () => {
